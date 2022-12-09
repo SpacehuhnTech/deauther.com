3545fd8e77737b0f8a530d16ab07041d3c9b8f10
Spacehuhn <github@spacehuhn.de>
2022-12-09 19:32:34 +0100

    Default dark mode :)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,6 +75,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
     }),
 };
 
