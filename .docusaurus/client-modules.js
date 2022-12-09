609301ba02d5f11cd7e6325dd90693b57d6d9050
Spacehuhn <github@spacehuhn.de>
2022-12-09 11:44:43 +0100

    Docusaurus basics

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Stefan\\Documents\\Projects\\deauther.com\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Stefan\\Documents\\Projects\\deauther.com\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Stefan\\Documents\\Projects\\deauther.com\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Stefan\\Documents\\Projects\\deauther.com\\src\\css\\custom.css'),
+];
